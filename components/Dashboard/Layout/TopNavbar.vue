012cafc712cc735e8f9671db913a865ff3cf7105
tomiknet <46049748+tomiknet@users.noreply.github.com>
2019-01-08 23:49:28 +0100

    Add files via upload

---
+++ components/Dashboard/Layout/TopNavbar.vue
@@ -0,0 +1,158 @@
+<template>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-minimize">
+        <button class="btn btn-fill btn-icon" @click="minimizeSidebar">
+          <i :class="$sidebar.isMinimized ? 'ti-menu-alt' : 'ti-more-alt'"></i>
+        </button>
+      </div>
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" :class="{toggled: $sidebar.showSidebar}" @click="toggleSidebar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar bar1"></span>
+          <span class="icon-bar bar2"></span>
+          <span class="icon-bar bar3"></span>
+        </button>
+        <a class="navbar-brand">{{this.$route.name}}</a>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="open">
+            <router-link to="/admin/stats" class="dropdown-toggle btn-magnify" data-toggle="dropdown">
+              <i class="ti-panel"></i>
+              <p>Stats</p>
+            </router-link>
+          </li>
+            <drop-down tag="li" title="5" icon="ti-bell">
+            <li><a href="#">Notification 1</a></li>
+            <li><a href="#">Notification 2</a></li>
+            <li><a href="#">Notification 3</a></li>
+            <li><a href="#">Notification 4</a></li>
+            <li><a href="#">Another notification</a></li>
+            </drop-down>
+          <li>
+            <div class="text-xs-center">
+            <v-menu
+                    v-model="menu"
+                    class="user"
+                    :close-on-content-click="false"
+                    :nudge-width="200"
+                    offset-y
+            >
+              <v-card
+                      slot="activator"
+                      class="photo"
+                      img="https://cdn.vuetifyjs.com/images/cards/girl.jpg"
+              ></v-card>
+
+              <v-card class="menu">
+                <v-list>
+                  <v-list-tile avatar>
+                    <v-list-tile-avatar>
+                      <img src="https://cdn.vuetifyjs.com/images/cards/girl.jpg" alt="Jane">
+                    </v-list-tile-avatar>
+
+                    <v-list-tile-content>
+                      <v-list-tile-title>Jane Leider</v-list-tile-title>
+                      <v-list-tile-sub-title>Founder of Vuetify.js</v-list-tile-sub-title>
+                    </v-list-tile-content>
+
+                    <v-list-tile-action>
+                      <v-btn
+                              :class="fav ? 'red--text' : ''"
+                              icon
+                              @click="fav = !fav"
+                      >
+                        <v-icon>favorite</v-icon>
+                      </v-btn>
+                    </v-list-tile-action>
+                  </v-list-tile>
+                </v-list>
+
+                <v-divider></v-divider>
+
+                <v-list>
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-switch v-model="message" color="purple"></v-switch>
+                    </v-list-tile-action>
+                    <v-list-tile-title>Lock Screen</v-list-tile-title>
+                  </v-list-tile>
+
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-switch v-model="hints" color="purple"></v-switch>
+                    </v-list-tile-action>
+                    <v-list-tile-title>Display #nsfw</v-list-tile-title>
+                  </v-list-tile>
+                </v-list>
+
+                <v-list>
+                  <v-list-tile
+                          v-for="(item, index) in items"
+                          :key="index"
+                          @click=""
+                  >
+                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                  </v-list-tile>
+                </v-list>
+
+              </v-card>
+            </v-menu>
+            </div>
+          </li>
+          <!--<li>
+            <router-link to="/admin/overview" class="btn-rotate">
+              <i class="ti-settings"></i>
+            </router-link>
+          </li>-->
+        </ul>
+        <form class="navbar-form navbar-right navbar-search-form" role="search">
+          <div class="input-group">
+            <input type="text" value="" class="form-control" placeholder="Search...">
+            <span class="input-group-addon"><i class="fa fa-search"></i></span>
+          </div>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        activeNotifications: false,
+        items: [
+          { title: 'My Profile' },
+          { title: 'Edit Porofile' },
+          { title: 'Settings' },
+          { title: 'Logout' }
+        ]
+      }
+    },
+    methods: {
+      capitalizeFirstLetter (string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+      },
+      toggleNotificationDropDown () {
+        this.activeNotifications = !this.activeNotifications
+      },
+      closeDropDown () {
+        this.activeNotifications = false
+      },
+      toggleSidebar () {
+        this.$sidebar.displaySidebar(!this.$sidebar.showSidebar)
+      },
+      hideSidebar () {
+        this.$sidebar.displaySidebar(false)
+      },
+      minimizeSidebar () {
+        this.$sidebar.toggleMinimize()
+      }
+    }
+  }
+
+</script>
+<style>
+
+</style>
---
+++ main.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import './pollyfills'
+import VueRouter from 'vue-router'
+import VueNotify from 'vue-notifyjs'
+import VeeValidate from 'vee-validate'
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+import App from './App.vue'
+import { store } from './store/store'
+import Vuetify from 'vuetify'
+import velocity from 'velocity-animate'
+
+// Plugins
+import GlobalComponents from './gloablComponents'
+import GlobalDirectives from './globalDirectives'
+import SideBar from './components/UIComponents/SidebarPlugin'
+
+// router setup
+import routes from './routes/routes'
+
+// library imports
+
+import './assets/sass/paper-dashboard.scss'
+import './assets/sass/element_variables.scss'
+import './assets/sass/demo.scss'
+import 'vuetify/dist/vuetify.min.css'
+
+
+import sidebarLinks from './sidebarLinks'
+// plugin setup
+Vue.use(VueRouter)
+Vue.use(GlobalDirectives)
+Vue.use(GlobalComponents)
+Vue.use(VueNotify)
+Vue.use(Vuetify)
+Vue.use(SideBar, {sidebarLinks: sidebarLinks})
+Vue.use(VeeValidate)
+locale.use(lang)
+
+// configure router
+const router = new VueRouter({
+  routes, // short for routes: routes
+  linkActiveClass: 'active',
+  mode: 'history'
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router,
+  store:store
+})
